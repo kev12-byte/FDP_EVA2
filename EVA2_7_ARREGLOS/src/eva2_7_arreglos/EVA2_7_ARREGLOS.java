b3ce7aa4b7171e8c7a43627583b390d43566af6f
kev12-byte <L25550590@chihuahua2.tecnm.mx>
2025-10-06 22:35:15 -0600

    Add files via upload

---
+++ EVA2_7_ARREGLOS/src/eva2_7_arreglos/EVA2_7_ARREGLOS.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_7_arreglos;
+
+/**
+ *
+ * @author Admin
+ */
+public class EVA2_7_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cade = "Hola mundo!!";
+        System.out.println(cade.charAt(0));
+        char caracter = cade.charAt(11);
+        System.out.println(caracter);
+        System.out.println(cade.length());
+        // FOR: CUANSO SABEMOS LA CANTIDAD DE REPETICIONES.
+        // WHILE: CUANDO PUEDEN SER 0 O N VECES.
+        // DO WHILE: CUANDO PUEDE SER UNA O N VECES.
+        
+        int valor = 10;
+    }
+    
+}
---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author Admin
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //todo se va a repetir dependiendo de lo que indique en el for
+        //variable y valor de inicio
+        //hasta donde llegas condicion --> tiene que ser verdadedra
+        // cuando es falso el for se detiene 
+        //incremento de conteo o decremento
+        //SUMAR O RESTAR VALORES
+        //OPERADORES: ANADIE1 O RESTAR 1
+        // ++  --
+        //i++ sicnifica sumarle 1 al valor de la i
+        for(int i = 1; i <= 10; i++){ 
+        System.out.println("HOLA MUNDO!!");
+        }
+        // i-- sicnifica restarle 1 al valor de la i
+        for(int i = 10; i >= 1; i--){ 
+        System.out.println("i");
+        }
+    }
+    
+}
